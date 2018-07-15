2be485a17ea9225a292563d9ba7f56d0fdb9e17d
macbook air <wangzhe@wangzhedeMacBook-Air.local>
2018-07-15 16:59:57 +0800

    create a directory

---
+++ chapter4/chapter4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef pseudocode
+/* 树的儿子兄弟表示法 */
+typedef struct TreeNode *PtrToNode
+
+struct TreeNode
+{
+	ElemenType Element;
+	PtrToNode FirstChild;
+	PtrToNode NextSibling;
+};
+#endif
+
+int main()
+{
+	return 1;
+}
