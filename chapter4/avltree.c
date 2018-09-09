9eb86fe8d86ea78c504977049496e9aa9dad9623
macbook air <wangzhe@wangzhedeMacBook-Air.local>
2018-09-09 17:02:54 +0800

    modified:   avltree.c 	new file:   test

--- chapter4/avltree.c
+++ chapter4/avltree.c
@@ -1,5 +1,8 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
 
 struct AvlNode;
 typedef struct AvlNode *Position;
@@ -65,6 +68,7 @@ Position FindMax(AvlTree T)
 	return T;
 }
 
+/* 封装一个返回节点高度的函数，不用费心处理空指针的情况 */
 static int Height(Position P)
 {
 	if (!P)
@@ -83,6 +87,7 @@ static void updateHeight(Position P)
 	P->Height = Max(Height(P->Left), Height(P->Right)) + 1;
 }
 
+/* 所有旋转函数中的空指针由外界保证 */
 static Position SingleRotateWithLeft(Position K2)
 {
 	Position K1 = K2->Left;
@@ -101,4 +106,59 @@ static Position SingleRotateWithRight(Position K1)
 	updateHeight(K2);
 	updateHeight(K1);
 	return K2;
+}
+
+static Position DoubleRotateWithLeft(Position K3)
+{
+	K3->Left = SingleRotateWithRight(K3->Left);
+	return SingleRotateWithLeft(K3);
+}
+
+static Position DoubleRotateWithRight(Position K3)
+{
+	K3->Right = SingleRotateWithLeft(K3->Right);
+	return SingleRotateWithRight(K3);
+}
+
+AvlTree Insert(ElementType X, AvlTree T)
+{
+	if (!T)
+	{
+		T = malloc(sizeof(struct AvlNode));
+		T->Left = T->Right = NULL;
+		T->Element = X;
+	}
+	else if (X < T->Element)
+	{
+		T->Left = Insert(X, T->Left);
+		if (Height(T->Left) - Height(T->Right) == 2)
+		{
+			if (X < T->Left->Element)
+				T = SingleRotateWithLeft(T);
+			else
+				T = DoubleRotateWithRight(T);
+		}
+	}
+	else if (X > T->Element)
+	{
+		T->Right = Insert(X, T->Right);
+		if (Height(T->Right) - Height(T->Left) == 2)
+		{
+			if (X < T->Right->Element)
+				T = SingleRotateWithRight(T);
+			else
+				T = DoubleRotateWithRight(T);
+		}
+	}
+	else
+	{
+	}
+	updateHeight(T);
+	return T;
+}
+
+
+int main()
+{
+	return 1;
 }
